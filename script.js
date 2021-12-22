94140b840708e279ec8c5de52bed8d05b8a6afb7
Rohit Pandey <rohitpandey1896@gmail.com>
2021-12-22 11:36:21 +0530

    reverting to original

--- script.js
+++ script.js
@@ -1,149 +1,193 @@
-var data = {
-    title: null,
-    page_content: null,
-    previousActiveLink: null,
-}
+    var intro = 'Hello, Im Rohit Im a full-stack developer.';
+    var flag = false;
+    var animateHobbiesFlag = false;
 
-const filesPath = {
-    welcomePage: 'content/welcome_page.txt',
-    aboutPage: 'content/about_page.txt',
-    workPage: 'content/work_page.txt',
+    function isOnScreen(elem, deltaOffset = 5){
+        if(elem.length == 0) return;
 
-}
-
-const linkIndex = {
-    null: 0,
-    homeLink : 0,
-    aboutLink: 1,
-    workLink: 2,
-}
-
-// load content on page laod
-async function loadContent() {
-    getActiveLink();
+        var $window = jQuery(window);
+        var viewport_top = $window.scrollTop();
+        var  viewport_height = $window.height();
+        var viewport_bottom =  viewport_top + viewport_height;
+        var $elem = jQuery(elem);
+        var top = $elem.offset().top + deltaOffset;
+        var height = $elem.height();
+        var bottom = top + height;
     
-}
 
+        return( bottom > viewport_top && top < viewport_bottom);
 
-
-function getActiveLink(activeTab = null) {
-    if(data.previousActiveLink) {
-        document.getElementById(data.previousActiveLink).style.color = "white";
     }
-    if (!activeTab) {
-        const url = window.location.href;
-        const section = url.split('#')[1];
-        activeTab = section ? section : 'home';
+
+    function writeIntro(elem, intro){
+        if(elem.length == 0 || intro.length == 0) return;
+        var $elem = jQuery(elem);
+        
+        for(let i=0; i < intro.length; i++){
+        
+
+        (function(i){
+
+            window.setTimeout(function(){
+            $elem.append(intro.charAt(i));
+            
+            if(i==15) $elem.append('<br>');
+            }, i * 40);
+        
+        }(i));
+        }
+        
+
     }
-    displaySection(activeTab);
-    
-    activeTab += 'Link';
-    readContentFromFile(linkIndex[activeTab]);
 
-    data.previousActiveLink = activeTab;
-    document.getElementById(activeTab).style.color = "orange";
-}
 
-function displaySection(activeTab){
-    if(data.previousActiveLink){        
-        const prevLink = data.previousActiveLink.substring(0, data.previousActiveLink.indexOf('Link'));
-        document.getElementById(prevLink).style.display = 'none';
+    function skillLevel(){
+        var maxLevelWidth = 80;
+        $('.skillLevel').each(function(){
+            if($(this).html() == 'angular'){
+                $(this).animate({width :maxLevelWidth * 70/100+'%'} ,1000);
+            }else if($(this).html() == 'spring'){
+                $(this).animate({width:maxLevelWidth * 60/100+'%'} ,600);
+            }else if($(this).html() == 'html'){
+                $(this).animate({width:maxLevelWidth * 71/100+'%'} ,200);
+            }else if($(this).html() == 'css'){
+                $(this).animate({width:maxLevelWidth * 72/100+'%'} ,700);
+            }else if($(this).html() == 'bootstrap'){
+                $(this).animate({width:maxLevelWidth * 68/100+'%'} ,500);
+            }else if($(this).html() == 'javascript'){
+                $(this).animate({width:maxLevelWidth * 45/100+'%'} ,300);
+            }else if($(this).html() == 'java'){
+                $(this).animate({width:maxLevelWidth * 77/100+'%'} ,350);
+            }else if($(this).html() == 'python'){
+                $(this).animate({width:maxLevelWidth * 68/100+'%'} ,800);
+            }else if($(this).html() == 'android'){
+                $(this).animate({width:maxLevelWidth * 30/100+'%'} ,800);
+            }else if($(this).html() == 'mysql'){
+                $(this).animate({width:maxLevelWidth * 40/100+'%'} ,1100);
+            }else if($(this).html() == 'c'){
+                $(this).animate({width:maxLevelWidth * 35/100+'%'} ,1400);
+            }
+        });
+
     }
-    document.getElementById(activeTab).style.display = 'block';
 
-}
+    function showHobbies(){
+        var animationDuration = 800;
+        $('#likes-container div ul li:nth-child(1) div').animate({left : '0' } , animationDuration,
+            function(){
+            $('#likes-container div ul li:nth-child(2) div').animate({left : '0' }, animationDuration,
+            function(){
+            $('#likes-container div ul li:nth-child(3) div').animate({left : '0' },animationDuration,
+            function(){
+            $('#likes-container div ul li:nth-child(4) div').animate({left : '0' },animationDuration);
+        });
+        } );
+        });
+        
+
+    }
+
+   
 
 
 
-async function readContentFromFile(index) {
-    const file = fetchFilePath(index);
-    await readTextFile(file).then((content) => {
-        data.page_content = content;
+
+    window.addEventListener('load', function(){
+        $('#headerOptionsBox').addClass('rightToLeftAnimation');
+        $('#profileIcon span' ).css('background-color','whitesmoke');
+
+        writeIntro('#introductionQuote', intro);
+        if(isOnScreen($('#likes-container'))) showHobbies();
+       // if(isOnScreen('#about')) $('#about #profileImage').addClass('profileImageEnlarge') ;
+        if(isOnScreen('#about')) $('#about .section-name span').addClass('section-name-span');
+        if(isOnScreen('#project')) $('#project .section-name span').addClass('section-name-span');
+        if(isOnScreen('#education-container')) $('#education-container .section-name span').addClass('section-name-span');
+        if(isOnScreen('#likes-container')) $('#likes-container .section-name span').addClass('section-name-span');
+        if(isOnScreen('#contact')) $('#contact .section-name span').addClass('section-name-span');
+
     });
-    setPageContent(index);
-}
 
-function setHomepageData(){
-    const name = fetchString(data.page_content, '<name>', '</name>');
-    const designation = fetchString(data.page_content, '<designation>', '</designation>');
-    const headline = fetchString(data.page_content, '<headline>', '</headline>');
-    writeData('id', 'name', name);
-    writeData('id', 'designation', designation);    
-    writeData('id', 'headline', headline);
+    $(document).ready( function(){
+            //$(this).scrollTop(0);
+            $('#profile button:nth-child(1)').addClass('leftToRightAnimation');
+            $('#profile button:nth-child(2)').addClass('rightToLeftAnimation');
+            $('#linkContainer').addClass('leftToRightAnimation');
+            
+            isOnScreen('#profile') ? $('#scrollTopButton div').hide() :$('#scrollTopButton div').show();
 
-}
+            //tooltip
+            $('[data-toggle="tooltip"]').tooltip();
 
-function setAboutPageData(){
-    const role = fetchString(data.page_content, '<role>', '</role>');
-    const about = fetchString(data.page_content, '<about>', '</about>');
-    writeData('id', 'role', role);    
-    writeData('id', 'about_desc', about);
-
-}
-
-function setWorkPageData(){
-    const work_intro = fetchString(data.page_content, '<work_intro>', '</work_intro>');
-   writeData('id', 'work_intro', work_intro); 
-
-}
-
-function setPageContent(index){
-    switch(index){
-        case 0:
-            setHomepageData();
-            break;
-        case 1:
-            setAboutPageData();
-            break;
-        case 2:
-            setWorkPageData();
-            break;
-    }
-
-}
+           // if(isOnScreen('#about')) $('#about #profileImage').addClass('profileImageEnlarge') ;
+            if(isOnScreen('#about')) $('#about .section-name span').addClass('section-name-span');
+            if(isOnScreen('#project')) $('#project .section-name span').addClass('section-name-span');
+            if(isOnScreen('#education-container')) $('#education-container .section-name span').addClass('section-name-span');
+            if(isOnScreen('#likes-container')) $('#likes-container .section-name span').addClass('section-name-span');
+            if(isOnScreen('#contact')) $('#contact .section-name span').addClass('section-name-span');
 
 
-function fetchString(strng, startSplitter, endSplitter) {
-    return strng.substring(
-        strng.indexOf(startSplitter)
-        + startSplitter.length,
-        strng.lastIndexOf(endSplitter)
-    );
-}
+            window.addEventListener('unload', function(){
+            writeIntro('#introductionQuote', intro);
+            $('#profile button').addClass('leftToRightAnimation');
+            $('#hackerRankBtn').addClass('rightToLeftAnimation');
+            $('#linkContainer').addClass('leftToRightAnimation');
+
+            
+        
+        });
+        
+
+    
+        window.addEventListener('scroll', function(){
+
+            isOnScreen('#profile') ? $('#scrollTopButton div').hide() :$('#scrollTopButton div').show();
+           // if(isOnScreen('#about',100)) $('#about #profileImage').addClass('profileImageEnlarge') ;
+            if(isOnScreen('#about', 200)) $('#about .section-name span').addClass('section-name-span');
+            if(isOnScreen('#project', 200)) $('#project .section-name span').addClass('section-name-span');
+            if(isOnScreen('#education-container', 200)) $('#education-container .section-name span').addClass('section-name-span');
+            if(isOnScreen('#likes-container', 200)) $('#likes-container .section-name span').addClass('section-name-span');
+            if(isOnScreen('#contact', 200)) $('#contact .section-name span').addClass('section-name-span');
+        // if(isOnScreen('#about ', 200)) $('#about .aboutQuote p').css({'background-color': 'white'});
 
 
+            if(isOnScreen($('#skillsContainer'), 420)) skillLevel();
 
-function fetchFilePath(index) {
-    switch (index) {
-        case 0:
-            return filesPath.welcomePage;
-        case 1:
-            return filesPath.aboutPage;
-        case 2:
-            return filesPath.workPage;
-    }
-}
+            if(isOnScreen($('#likes-container'), 420)) showHobbies();
+            
+            
+        });
 
 
-function readTextFile(file) {
-    return fetch(file)
-        .then(response => response.text());
-}
+        
+        //sending Email
+    $('#sendEmailBtn').click(function(event){
+        
+        var name = $('form #name').val();
+        var subject = $('form #subject').val();
+        var body = $('form #body').val();
+        $('form #name').css({'background-color': '#1e272c'});
+        $('form #subject').css({'background-color': '#1e272c'});
+        $('form #body').css({'background-color': '#1e272c'});
 
-// write data to html
-function writeData(selectorType, selector, content) {
-    switch (selectorType) {
-        case 'id':
-            document.getElementById(selector).innerHTML = content;
-            break;
-        case 'class':
-            document.getElementsByClassName(selector).innerHTML = content;
-            break;
-        case 'tag':
-            document.getElementsByTagName(selector).innerHTML = content;
-            break;
+        if(name.length == 0){
+            $('#statusMessage').text('*Please Enter Your Name');
+            $('form #name').css({'background-color': 'lightslategrey'});
+            event.preventDefault();
+        }else if(subject.length == 0){
+            $('#statusMessage').text('*Please Enter Subject');
+            $('form #subject').css({'background-color': 'lightslategrey'});
+            event.preventDefault();
+        }else if(body.length == 0){
+            $('#statusMessage').text('*Please Write Your Message');
+            $('form #body').css({'background-color': 'lightslategrey'});
+            event.preventDefault();
+        }else{
+            var mailLink = 'mailto:rdxi77@gmail.com?subject='+subject+'&body='+'Name : '+name+', Message : '+body;
+            $('#sendEmailBtn').attr("href",mailLink);
+        }
 
 
-    }
-
-}
+        
+    });
+        
+    });
