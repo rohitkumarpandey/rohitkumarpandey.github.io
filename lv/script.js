a0fa591f72bf752a1d30108f8584600fb50b2c09
Rohit Pandey <rohitpandey@Rohits-MacBook-Air.local>
2024-12-29 18:32:19 +0530

    updated instructions screenshots

--- lv/script.js
+++ lv/script.js
@@ -1,27 +1,31 @@
 const steps = {
     'step-1': {
-        title: 'Step 1: Open the Layout Viewer',
-        description: 'To open the Layout Viewer, click on the Layout Viewer icon in the toolbar.',
+        title: 'Step 1: Add Layout Viewer Chrome extension',
+        description: 'Add the Layout Viewer Chrome extension to your browser by clicking on the <strong>Add to Chrome</strong> button.',
     },
     'step-2': {
-        title: 'Step 2: Select the Layout',
-        description: 'Select the layout you want to view from the list of layouts.',
+        title: 'Step 2: Layout Viewer Added',
+        description: 'Refresh the page, the Layout Viewer extension has been added to your browser. You can now view layouts of your developing website.',
     },
     'step-3': {
-        title: 'Step 3: View the Layout',
-        description: 'The selected layout will be displayed in the Layout Viewer.',
+        title: 'Step 3: Layout Viewer',
+        description: 'Click on the Layout Viewer button in the browser screen to open the Layout Viewer.',
     },
     'step-4': {
-        title: 'Step 4: Navigate the Layout',
-        description: 'Use the navigation buttons to zoom in/out, move, and rotate the layout.',
+        title: 'Step 4: View the Layout',
+        description: 'The Layout Viewer will display the layout of the website you are developing. It has a list of devices to view the layout in.',
     },
     'step-5': {
-        title: 'Step 5: Close the Layout Viewer',
-        description: 'To close the Layout Viewer, click on the close button.',
+        title: 'Step 5: Full Screen Layout Viewer',
+        description: 'To view the layout in full screen, click on the full screen button.',
     },
     'step-6': {
-        title: 'Step 6: Provide Feedback',
-        description: 'If you have any feedback or suggestions, click on the feedback button to send us an email.',
+        title: 'Step 6: Add a new tab',
+        description: 'Click on <strong>+</strong> button to add a new layout tab.',
+    },
+    'step-7': {
+        title: 'Step 7: Choose Orientation',
+        description: 'Choose the orientation of the device.',
     },
 }
 
@@ -48,7 +52,7 @@ function showInstructions(step) {
     const instructionDescription = document.getElementById('description');
     if (instruction && instructionStep && instructionDescription) {
         instructionStep.textContent = instruction.title;
-        instructionDescription.textContent = instruction.description;
+        instructionDescription.innerHTML = instruction.description;
     }
 }
 function showDivs(n) {
